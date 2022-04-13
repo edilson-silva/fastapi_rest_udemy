77bee53347529dfc2274295a76e1cf193472565d
Edilson Silva <eddysilva1994@gmail.com>
2022-04-13 19:17:30 -0300

    feat: Add database connection

---
+++ complete_fastapi_rest_course_+_aws_+_emails_+_payments/connection/database.py
@@ -0,0 +1,17 @@
+from os import getenv
+
+from dotenv import find_dotenv, load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv(find_dotenv())
+
+
+DATABASE_URL = getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
