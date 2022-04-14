5ba96fc9cdaf1af643ee7b29912a4cd99b8a00e6
Edilson Silva <eddysilva1994@gmail.com>
2022-04-14 01:27:45 -0300

    feat: Add method to retrieve database connection

--- complete_fastapi_rest_course_+_aws_+_emails_+_payments/connection/database.py
+++ complete_fastapi_rest_course_+_aws_+_emails_+_payments/connection/database.py
@@ -15,3 +15,12 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+def get_db_connection():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
