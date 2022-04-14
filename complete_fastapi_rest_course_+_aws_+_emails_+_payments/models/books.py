e39432d773a95575358f8fffeab2804032b83353
Edilson Silva <eddysilva1994@gmail.com>
2022-04-14 00:55:00 -0300

    feat: Add books model

---
+++ complete_fastapi_rest_course_+_aws_+_emails_+_payments/models/books.py
@@ -0,0 +1,10 @@
+from connection.database import Base
+from sqlalchemy import Column, Integer, String
+
+
+class BookSchema(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    author = Column(String, nullable=False)
