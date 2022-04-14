cb00b8a79e990322f5a5f7b9c98c961da796c5df
Edilson Silva <eddysilva1994@gmail.com>
2022-04-14 19:18:22 -0300

    refact: Import session maker from sqlalchemy.orm instead of sqlalchemy.ext.declarative

--- complete_fastapi_rest_course_+_aws_+_emails_+_payments/connection/database.py
+++ complete_fastapi_rest_course_+_aws_+_emails_+_payments/connection/database.py
@@ -2,8 +2,7 @@ from os import getenv
 
 from dotenv import find_dotenv, load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 load_dotenv(find_dotenv())
 
