806ac38931087c51ea802b1aae63bd0f9dfcdcec
Edilson Silva <eddysilva1994@gmail.com>
2022-04-24 15:06:30 -0300

    feat: Added database provisioning on app startup event

--- src/connection/database.py
+++ src/connection/database.py
@@ -16,6 +16,10 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
+def init_db():
+    Base.metadata.create_all(engine)
+
+
 def get_db_connection():
     db = SessionLocal()
 
