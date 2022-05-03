6879c639d5a6300fd3ab59f7fd7dfca1508cdb6b
Edilson Silva <eddysilva1994@gmail.com>
2022-05-03 19:31:22 -0300

    fix: Duplicated table readers name

--- src/models/readers_books.py
+++ src/models/readers_books.py
@@ -3,7 +3,7 @@ from src.connection.database import Base
 
 
 class ReaderBookModel(Base):
-    __tablename__ = "readers"
+    __tablename__ = "readers_books"
 
     id = Column(Integer, primary_key=True, index=True)
     book_id = Column(ForeignKey("books.id"), nullable=False)
