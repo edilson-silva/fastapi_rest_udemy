84c080ee8a45b83c300ae8f48f236a501d92a487
Edilson Silva <eddysilva1994@gmail.com>
2022-05-03 19:25:46 -0300

    feat: Add readers and books relationship

--- src/models/books.py
+++ src/models/books.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, Integer, String
 from src.connection.database import Base
 
 
@@ -8,4 +8,3 @@ class BookModel(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, nullable=False)
     author = Column(String, nullable=False)
-    reader_id = Column(ForeignKey("readers.id"), nullable=False, index=True)
