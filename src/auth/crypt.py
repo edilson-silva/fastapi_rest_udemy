b0da9fe6aecf36c14cbb82c7f7648aee0497adc8
Edilson Silva <eddysilva1994@gmail.com>
2022-05-04 01:10:42 -0300

    feat: Add auth password crypt util

---
+++ src/auth/crypt.py
@@ -0,0 +1,10 @@
+from passlib.context import CryptContext
+
+
+class Crypt:
+    def __init__(self, password) -> None:
+        self.__pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+        self.password = password
+
+    def get_hashed_password(self):
+        return self.__pwd_context.hash(self.passowrd)
