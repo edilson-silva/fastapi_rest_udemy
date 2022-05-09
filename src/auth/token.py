5994e47faab838045356f353a26c636b8f260b1f
Edilson Silva <eddysilva1994@gmail.com>
2022-05-08 23:47:12 -0300

    refactor: Add and set token algorithm in token generation

--- src/auth/token.py
+++ src/auth/token.py
@@ -1,9 +1,10 @@
 from datetime import datetime, timedelta
-from email import message
 from os import environ
 
 from jwt import encode as jwt_encode
 from src.schemas.users import UserSchema
+
+TOKEN_ALGORITHM = "HS256"
 
 
 def generate_token(user: UserSchema) -> str:
@@ -17,6 +18,10 @@ def generate_token(user: UserSchema) -> str:
     """
     try:
         payload = {"sub": user.id, "exp": datetime.utcnow() + timedelta(minutes=5)}
-        return jwt_encode(payload=payload, key=environ.get("JWT_SECRET_KEY"))
+        return jwt_encode(
+            payload=payload,
+            key=environ.get("JWT_SECRET_KEY"),
+            algorithm=TOKEN_ALGORITHM,
+        )
     except Exception:
         raise Exception(message="Token generation error")
