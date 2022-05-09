89bffe24138f2c4c8314b13a4b6d64f793c5c6e7
Edilson Silva <eddysilva1994@gmail.com>
2022-05-09 01:14:37 -0300

    refactor: Set reader book create (Association) statuscode from status constants

--- src/main.py
+++ src/main.py
@@ -155,7 +155,8 @@ async def readers_books_create(
         return reader_book_model
     except IntegrityError:
         return JSONResponse(
-            content={"message": "send a valid reader and book ids"}, status_code=400
+            content={"message": "send a valid reader and book ids"},
+            status_code=status.HTTP_400_BAD_REQUEST,
         )
 
 
